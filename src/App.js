2e7e222428233c258f831d1797526bdb169c7817
Mohammed Ramadan <mohammed.ramadanko@gmail.com>
2020-09-23 03:13:42 +0200

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
-function App() {
+export default function App() {
+  // function renderProduct(p) {
+  //   return (
+  //     <div key={p.id} className="product">
+  //       <a href="/">
+  //         <img src={`/images/${p.image}`} alt={p.name} />
+  //         <h3>{p.name}</h3>
+  //         <p>${p.price}</p>
+  //       </a>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{" "}
+            <select id="size">
+              <option value="">All sizes</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+          </section>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
-
-export default App;
