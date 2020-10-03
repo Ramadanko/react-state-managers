f4f2521723a494419b5e0bcfaf10dbc0bb53505b
Mohammed Ramadan <mohammed.ramadanko@gmail.com>
2020-10-03 08:36:25 +0200

    add useFetchAll hook

---
+++ src/services/useFetchAll.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function useFetchAll(urls) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Build an array of requestes
+    const promises = urls.map((url) =>
+      fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
+        if (response.ok) return response.json();
+        throw response;
+      })
+    );
+
+    // Promise.all runs multiple promises at the same time. So this will make multiple HTTP requests at the same time
+    // it takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises
+    Promise.all(promises)
+      .then((json) => setData(json))
+      .catch((e) => {
+        console.error(e);
+        setError(e);
+      })
+      .finally(() => setLoading(false));
+    // eslint-disable-next-line
+  }, []);
+
+  return { data, loading, error };
+}
